8a0db0c1263e26b72e12f55c1bd7f2ed8d9db877
barteq <bartek2001@spoko.pl>
2021-03-28 11:22:19 +0200

    Initial commit

---
+++ src/test/java/com/barteq/restaurantReservationSystem/RestaurantReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barteq.restaurantReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

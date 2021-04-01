ce0c72b0cf713fafd1412848c8d27241579fc990
barteq <bartek2001@spoko.pl>
2021-04-01 13:17:13 +0200

    Main application logic done. Working on exception handling...

--- src/main/java/com/barteq/restaurantReservationSystem/RestaurantReservationSystemApplication.java
+++ src/main/java/com/barteq/restaurantReservationSystem/RestaurantReservationSystemApplication.java
@@ -2,9 +2,12 @@ package com.barteq.restaurantReservationSystem;
 
 import com.barteq.restaurantReservationSystem.entity.Order;
 import com.barteq.restaurantReservationSystem.service.OrderService;
+import com.barteq.restaurantReservationSystem.service.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RestaurantReservationSystemApplication {
@@ -15,10 +18,21 @@ public class RestaurantReservationSystemApplication {
 
 	@Autowired
 	private OrderService orderService;
+	@Autowired
+	private TableService tableService;
 
+
+	@PostConstruct
 	public void init() {
 		Order order = new Order();
-
+		order.setId(1);
+		order.setClientName("Marek Jakubiak");
+		order.setEmail("marek@gmail.com");
+		order.setAddress("Ludowa 12");
+		order.setCity("Kraków");
+		order.setZipCode("42-800");
+		order.setNumberOfPeople(2);
+		order.setTable(tableService.findById(2));
 		orderService.save(order);
 	}
 }
